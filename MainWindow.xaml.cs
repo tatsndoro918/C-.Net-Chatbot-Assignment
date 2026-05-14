182ad92bac878b3956cf0be51dab8826d099bf10
Tatenda Ndoro <tatsndoro918@gmail.com>
2026-05-14 16:46:26 +0200

    MainWindowxaml.cs: Added state flags, colur brushes, constructor and enter key shortcut

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Cybersecurity_Awareness_Bot
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        //  STATE FLAGS
+        private int _nameStep = 0;
+        private int _favTopicStep = 0;
+
+        // Colour brushes
+        private readonly SolidColorBrush _goldBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD700"));
+        private readonly SolidColorBrush _purpleBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#AA88FF"));
+        private readonly SolidColorBrush _textBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F5F0E8"));
+        private readonly SolidColorBrush _dimBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#AAAAAA"));
+
+        //  CONSTRUCTOR
         public MainWindow()
         {
             InitializeComponent();
+            Bot.PlayIntroSound();
+            AppendBotMessage(Bot.Welcome());
+            InputBox.Focus();
         }
+
+        //  ENTER KEY SHORTCUT
+        private void InputBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                SendButton_Click(sender, e);
+        }
+
     }
 }
